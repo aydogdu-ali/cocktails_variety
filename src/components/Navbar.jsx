06ab18b68db25c54d707f6403aa000bf10bd31b4
Ali <ali.ozgur@example.com>
2023-02-14 04:16:00 +0300

    edit navbar components and added css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import  logo from "../image/logo.svg" // Kullanacağımız logoyu import ettik.
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="nav-center">
+        {/*logo ya tıklanınca anasayfaya yönlendirdik.*/}
+        <Link to="/">
+          <img className="logo" src={logo} alt="coctail db logo" />
+        </Link>
+        <ul className="nav-links">
+          {/*Navbar da gösterdiğim sayfalara gitmek  için yönlendirme yaptık.*/}
+          <li>
+            <Link to="/"> Home</Link>
+          </li>
+          <li>
+            <Link to="/about"> About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
