f429832aac3e09a0858281ce3b50ceae9b8d05cc
Ali <ali.ozgur@example.com>
2023-02-14 04:58:53 +0300

    edit error pages and added css

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+// useNaviget Hook'u yönlendirme amacıyla kullandık.
+//Bu örnekte kullanıcı yanlış bir sayfa yönlendiğinde back butonu ile bir önceki sayfaya gelir.
+// Bunu alternatif olarak Link ile de yapabilirdik. Ancak aşağıdada görüldüğü gibi direkt sayfaya yönlendirmiş oluruz.
 
 const Error = () => {
+    const navigate = useNavigate();
   return (
-    <div>Error</div>
-  )
+    <section className=" section error-page">
+      <div className="error">
+        <h1>Oops! something went wrong please try again </h1>
+        <button className="btn btn-primary" onClick={()=> navigate(-1)}>back </button>
+        {/* <Link to ="/">back</Link> */}
+      </div>
+    </section>
+  );
 }
 
 export default Error
