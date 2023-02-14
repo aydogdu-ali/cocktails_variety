0a70455eab5b74783ca659b3d3eab50cb2553564
Ali <ali.ozgur@example.com>
2023-02-14 04:23:55 +0300

    edit about page and add css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='section about-section'>
+        
+        <h1 className='section-title'>About Us</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ea quibusdam at, nobis vero saepe voluptatum! Quidem officiis saepe iure maxime. In delectus repellendus possimus pariatur nostrum recusandae. Voluptates eligendi laboriosam rerum consequatur accusamus perferendis ea id delectus totam temporibus! Libero culpa pariatur excepturi omnis alias saepe vitae deleniti quae?</p>
+        </section>
   )
 }
 
