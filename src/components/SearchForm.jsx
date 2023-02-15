9021b8ec407627c7ee54f57f726f371c04b84e79
Ali <ali.ozgur@example.com>
2023-02-15 03:46:52 +0300

    edit searcForm component and add css

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useGlobalContext } from '../contex/contex'
 
 const SearchForm = () => {
+  const [text, setText] = useState("")
   const { setSearchText } = useGlobalContext();
+
+  const search =(e)=>{
+    setText(e.target.value)
+
+  }
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+setSearchText(text);
+setText("")
+  }
   return (
-    <div>SearchForm</div>
+    <section className='section- search'>
+      <form action="" className='search-form' onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search</label>
+          <input type="text" id="name" value={text} onChange={search}/>
+        </div>
+      </form>
+    </section>
   )
 }
 
